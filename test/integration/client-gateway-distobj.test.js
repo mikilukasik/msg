f107bce6fff3c6cdf772341e556fc5c96bdcc0a7
Miklos Lukasik <miklosbacsi@gmail.com>
2021-04-21 02:00:52 +0100

    more distobj tests

--- test/integration/client-gateway-distobj.test.js
+++ test/integration/client-gateway-distobj.test.js
@@ -17,7 +17,203 @@ let nextPortBase = 22000;
 
 const getLogger = (prefix) => (...args) => console.log(prefix, ...args);
 
-describe('client .do and .on, response with comms.send()', () => {
+describe('client <--> service distObj', () => {
+  it('client distobj gets initial state from service', async() => {
+    const serviceSocket = msg.service.ws(socketRoute);
+    const serviceDistObj = serviceSocket.distObj(distObjName);
+    serviceDistObj.data.testKey = 'testData';
+
+    const clientResponse = await msg.runOnClient(async({
+      nextPortBase,
+      socketRoute,
+      distObjName,
+    }) => {
+      const testSocket = msgClient.ws(`ws://0.0.0.0:${nextPortBase}${socketRoute}`);
+      const store = {};
+      testSocket.distObj({
+        name: distObjName,
+        store,
+      });
+
+      await new Promise(r => setTimeout(r, 250));
+      return store;
+    }, {
+      nextPortBase,
+      socketRoute,
+      distObjName,
+    });
+
+    expect(clientResponse).toStrictEqual({ testKey: 'testData' });
+  });
+
+  it('client distobj picks up change from service', async() => {
+    const serviceSocket = msg.service.ws(socketRoute);
+    const serviceDistObj = serviceSocket.distObj(distObjName);
+    serviceDistObj.data.testKey = 'testData';
+
+    const clientFirstResponse = await msg.runOnClient(async({
+      nextPortBase,
+      socketRoute,
+      distObjName,
+    }) => {
+      const testSocket = msgClient.ws(`ws://0.0.0.0:${nextPortBase}${socketRoute}`);
+      window.store = {};
+      testSocket.distObj({
+        name: distObjName,
+        store,
+      });
+
+      await new Promise(r => setTimeout(r, 250));
+      return store;
+    }, {
+      nextPortBase,
+      socketRoute,
+      distObjName,
+    });
+
+    expect(clientFirstResponse).toStrictEqual({ testKey: 'testData' });
+
+    serviceDistObj.data.secondKey = 'secondData';
+
+    const clientSecondResponse = await msg.runOnClient(async() => {
+      await new Promise(r => setTimeout(r, 250));
+      return store;
+    });
+
+    expect(clientSecondResponse).toStrictEqual({ testKey: 'testData', secondKey: 'secondData' });
+  });
+
+  xit('client distobj picks up nested change from service', async() => {
+    const serviceSocket = msg.service.ws(socketRoute);
+    const serviceDistObj = serviceSocket.distObj(distObjName);
+    serviceDistObj.data.testKey = { root: { deepData: 5 }};
+
+    const clientFirstResponse = await msg.runOnClient(async({
+      nextPortBase,
+      socketRoute,
+      distObjName,
+    }) => {
+      const testSocket = msgClient.ws(`ws://0.0.0.0:${nextPortBase}${socketRoute}`);
+      window.store = {};
+      testSocket.distObj({
+        name: distObjName,
+        store,
+      });
+
+      await new Promise(r => setTimeout(r, 250));
+      return store;
+    }, {
+      nextPortBase,
+      socketRoute,
+      distObjName,
+    });
+
+    // TODO: this already fails here
+    expect(clientFirstResponse).toStrictEqual({ root: { deepData: 5 }});
+
+    serviceDistObj.data.testKey.root.deepData = 'secondData';
+
+    const clientSecondResponse = await msg.runOnClient(async() => {
+      await new Promise(r => setTimeout(r, 250));
+      return store;
+    });
+
+    expect(clientSecondResponse).toStrictEqual({ root: { deepData: 'secondData' }});
+  });
+
+  it('service distobj picks up keys from a newly joined client', async() => {
+    const serviceSocket = msg.service.ws(socketRoute);
+    const serviceDistObj = serviceSocket.distObj(distObjName);
+
+    await msg.runOnClient(async({
+      nextPortBase,
+      socketRoute,
+      distObjName,
+    }) => {
+      const testSocket = msgClient.ws(`ws://0.0.0.0:${nextPortBase}${socketRoute}`);
+      const store = { fromClientStrore: 'test' };
+      testSocket.distObj({
+        name: distObjName,
+        store,
+      });
+
+      await new Promise(r => setTimeout(r, 250));
+    }, {
+      nextPortBase,
+      socketRoute,
+      distObjName,
+    });
+
+    expect(serviceDistObj.data).toStrictEqual({ fromClientStrore: 'test' });
+  });
+
+  it('service distobj picks up change from client', async() => {
+    const serviceSocket = msg.service.ws(socketRoute);
+    const serviceDistObj = serviceSocket.distObj(distObjName);
+
+    await msg.runOnClient(async({
+      nextPortBase,
+      socketRoute,
+      distObjName,
+    }) => {
+      const testSocket = msgClient.ws(`ws://0.0.0.0:${nextPortBase}${socketRoute}`);
+      const store = { fromClientStrore: 'test' };
+      window.clientDistObj = testSocket.distObj({
+        name: distObjName,
+        store,
+      });
+
+      await new Promise(r => setTimeout(r, 250));
+    }, {
+      nextPortBase,
+      socketRoute,
+      distObjName,
+    });
+
+    expect(serviceDistObj.data).toStrictEqual({ fromClientStrore: 'test' });
+
+    await msg.runOnClient(async() => {
+      window.clientDistObj.data.secondKey = 'secondData'
+      await new Promise(r => setTimeout(r, 250));
+    });
+
+    expect(serviceDistObj.data).toStrictEqual({ fromClientStrore: 'test', secondKey: 'secondData' });
+  });
+
+  it('service distobj picks up nested change from client', async() => {
+    const serviceSocket = msg.service.ws(socketRoute);
+    const serviceDistObj = serviceSocket.distObj(distObjName);
+
+    await msg.runOnClient(async({
+      nextPortBase,
+      socketRoute,
+      distObjName,
+    }) => {
+      const testSocket = msgClient.ws(`ws://0.0.0.0:${nextPortBase}${socketRoute}`);
+      const store = { fromClientStrore: { rootKey: { deepKey: 'test'} } };
+      window.clientDistObj = testSocket.distObj({
+        name: distObjName,
+        store,
+      });
+
+      await new Promise(r => setTimeout(r, 250));
+    }, {
+      nextPortBase,
+      socketRoute,
+      distObjName,
+    });
+
+    expect(serviceDistObj.data).toStrictEqual({ fromClientStrore: { rootKey: { deepKey: 'test'} } });
+
+    await msg.runOnClient(async() => {
+      window.clientDistObj.data.fromClientStrore.rootKey.deepKey = 'secondData';
+      await new Promise(r => setTimeout(r, 250));
+    });
+
+    expect(serviceDistObj.data).toStrictEqual({ fromClientStrore: { rootKey: { deepKey: 'secondData'} } });
+  });
+
+
   before(async() => {
     client = getClient();
     await client.start();
@@ -69,32 +265,5 @@ describe('client .do and .on, response with comms.send()', () => {
 
   after(async() => {
     await client.stop();
-  });
-
-  it('service distobj picks up keys from a newly joined client', async() => {
-    const serviceSocket = msg.service.ws(socketRoute);
-    const serviceDistObj = serviceSocket.distObj(distObjName);
-
-    await msg.runOnClient(async({
-      nextPortBase,
-      socketRoute,
-      distObjName,
-    }) => {
-      const testSocket = msgClient.ws(`ws://0.0.0.0:${nextPortBase}${socketRoute}`);
-      const store = { fromClientStrore: 'test' };
-      const clientDistObj = testSocket.distObj({
-        name: distObjName,
-        store,
-      });
-
-      await new Promise(r => setTimeout(r, 250));
-      return { store, clientDistObj };
-    }, {
-      nextPortBase,
-      socketRoute,
-      distObjName,
-    });
-
-    expect(serviceDistObj.data).toStrictEqual({ fromClientStrore: 'test' });
   });
 });
