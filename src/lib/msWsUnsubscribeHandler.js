d71f7c036af0a4f4bfe20617ac1cf031df635a34
Miklos Lukasik <miklosbacsi@gmail.com>
2021-05-10 01:45:25 +0100

    quickfix

--- src/lib/msWsUnsubscribeHandler.js
+++ src/lib/msWsUnsubscribeHandler.js
@@ -7,6 +7,8 @@ module.exports = function handlerCreator(msgOptions, route) {
     var event = data.event;
     if (!event || event === 'undefined') log( new Error('event cannot be undefined') )
 
+    if (!msgOptions.wsRoutes[route].subscriptions[event]) return;
+
     const indexToRemove = msgOptions.wsRoutes[route].subscriptions[event].findIndex(({ key }) => key === comms.key);
     const subscribeHandler = msgOptions.wsRoutes[route].subscriptions[event][indexToRemove];
     // TODO: subscribehandler sometimes is undefined. wonder why..
