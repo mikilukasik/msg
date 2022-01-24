8b8eaf673e266ed863a3d6d1cf732f2870c68bb1
mikilukasik <szarhazi@gmail.com>
2022-01-24 19:53:08 +0000

    cors

--- src/service.js
+++ src/service.js
@@ -1,26 +1,26 @@
 const getRandomId = require('./lib/getRandomId.js');
 
-module.exports = function createMsgService(optionalOptions){
-  var DEV = ['development', 'dev'].indexOf( process.env.NODE_ENV ) >= 0 
+module.exports = function createMsgService(optionalOptions) {
+  var DEV = ['development', 'dev'].indexOf(process.env.NODE_ENV) >= 0;
 
   var log = console.log;
   // process.on('unhandledRejection', ur => {
   //   log('unhandledRejection', ur);
   // });
-  
+
   var fs = require('fs');
   var path = require('path');
-  
+  var cors = require('cors');
+
   var shareFile = () => log('!!!PURE FUNCTION shareFile');
   // if (!DEV) {
   //   log('Starting memwatch, will dump and serve heapfiles on memory leaks.');
   //   var memwatch = require('memwatch-next');
-  //   var heapdump = require('heapdump');  
-  
-  
+  //   var heapdump = require('heapdump');
+
   //   memwatch.on('leak', function(info) {
   //     log('!!!!! MEMORY LEAK IN MSG GATEWAY !!!!!', info, 'Dumping heap in file...');
-  //     heapdump.writeSnapshot(function(err, _filename) {  
+  //     heapdump.writeSnapshot(function(err, _filename) {
   //       if (err) return log(err);
   //       const filename = path.resolve(_filename);
   //       log('dump written to', filename);
@@ -28,7 +28,7 @@ module.exports = function createMsgService(optionalOptions){
   //     });
   //   });
   // }
-  
+
   var express = require('express');
   var cookieParser = require('cookie-parser');
   var bodyParser = require('body-parser');
@@ -38,15 +38,17 @@ module.exports = function createMsgService(optionalOptions){
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(cookieParser());
+  app.use(cors());
 
   if (typeof optionalOptions === 'undefined') optionalOptions = {};
-  if (typeof optionalOptions !== 'object') throw new Error('msgService string constructor not implemented: ' + optionalOptions);
+  if (typeof optionalOptions !== 'object')
+    throw new Error('msgService string constructor not implemented: ' + optionalOptions);
 
   var msgOptions = {
     startedAt: new Date(),
-    DEV: ['development', 'dev'].indexOf( process.env.NODE_ENV ) >= 0,
+    DEV: ['development', 'dev'].indexOf(process.env.NODE_ENV) >= 0,
     log: log,
-    express:express,
+    express: express,
     app: app,
     ws: new WebSocketClient(),
     PORT: 5000,
@@ -63,11 +65,15 @@ module.exports = function createMsgService(optionalOptions){
     intervalIds: {},
     stopped: false,
 
-    connection: { send: function(){ log('pure method: connection.send'); } },
+    connection: {
+      send: function () {
+        log('pure method: connection.send');
+      },
+    },
     ip: {
       private: '',
       public: '',
-      gateway: ''
+      gateway: '',
     },
     gotIp: false,
     retryCount: {},
@@ -75,20 +81,22 @@ module.exports = function createMsgService(optionalOptions){
     subscribedTo: {},
   };
 
-  Object.keys(optionalOptions).forEach(function(key){
+  Object.keys(optionalOptions).forEach(function (key) {
     msgOptions[key] = optionalOptions[key];
-    if(key === 'log') log = optionalOptions.log;
+    if (key === 'log') log = optionalOptions.log;
   });
 
   msgOptions.callWhenConnected = (confirmConnect) => {
     if (msgOptions.connected && msgOptions.gotIp) {
       return confirmConnect();
     }
-    msgOptions.timeoutIds.callWhenConnected = setTimeout(function(){msgOptions.callWhenConnected(confirmConnect);}, 200);   // TODO: trigger this on connection instead of setTimeout
-  }
+    msgOptions.timeoutIds.callWhenConnected = setTimeout(function () {
+      msgOptions.callWhenConnected(confirmConnect);
+    }, 200); // TODO: trigger this on connection instead of setTimeout
+  };
 
-  msgOptions.waitForConnection = function(){
-    return new Promise(function(res4, rej4){
+  msgOptions.waitForConnection = function () {
+    return new Promise(function (res4, rej4) {
       msgOptions.callWhenConnected(res4);
     });
   };
@@ -96,17 +104,17 @@ module.exports = function createMsgService(optionalOptions){
   msgOptions.serviceLongName = msgOptions.serviceName + '-' + msgOptions.serviceId;
 
   msgOptions.getIps = require('./lib/getIps')(msgOptions);
-  msgOptions.toGtw = require('./lib/toGtw')(msgOptions); 
-  msgOptions.myCallBacks = require('./lib/myCallBacks')(msgOptions); 
-  msgOptions.askGtw = require('./lib/askGtw')(msgOptions); 
-  msgOptions.sendRule = require('./lib/sendRule')(msgOptions); 
-  msgOptions.cacheRule = require('./lib/cacheRule')(msgOptions); 
-  msgOptions.registerRule = require('./lib/registerRule')(msgOptions); 
-  msgOptions.createHttpRule = require('./lib/createHttpRule')(msgOptions); 
-  msgOptions.createSocketRule = require('./lib/createSocketRule')(msgOptions); 
-  msgOptions.createRule = require('./lib/createRule')(msgOptions); 
+  msgOptions.toGtw = require('./lib/toGtw')(msgOptions);
+  msgOptions.myCallBacks = require('./lib/myCallBacks')(msgOptions);
+  msgOptions.askGtw = require('./lib/askGtw')(msgOptions);
+  msgOptions.sendRule = require('./lib/sendRule')(msgOptions);
+  msgOptions.cacheRule = require('./lib/cacheRule')(msgOptions);
+  msgOptions.registerRule = require('./lib/registerRule')(msgOptions);
+  msgOptions.createHttpRule = require('./lib/createHttpRule')(msgOptions);
+  msgOptions.createSocketRule = require('./lib/createSocketRule')(msgOptions);
+  msgOptions.createRule = require('./lib/createRule')(msgOptions);
 
-  msgOptions.obj = require('./lib/msgServiceObj')(msgOptions); 
+  msgOptions.obj = require('./lib/msgServiceObj')(msgOptions);
   msgOptions.obj.app = app;
 
   // shareFile = function(filename){
