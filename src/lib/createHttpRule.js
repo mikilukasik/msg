d706b33d262c458701e37428f12d4f433150945f
mikilukasik <szarhazi@gmail.com>
2022-11-27 18:45:00 +0000

    .

--- src/lib/createHttpRule.js
+++ src/lib/createHttpRule.js
@@ -1,12 +1,11 @@
-module.exports = function createHttpRuleCreator(msgOptions){
-
-  return function createHttpRule(argObj, method, rule = {}){
+module.exports = function createHttpRuleCreator(msgOptions) {
+  return function createHttpRule(argObj, method, rule = {}) {
     rule.method = method;
     var pOptions = {
-      url: argObj.cmd
+      url: argObj.cmd,
     };
 
-    if (pOptions.url[0] === '/'){
+    if (pOptions.url[0] === '/') {
       rule.type = 'httpRedirect';
       rule.inPath = pOptions.url;
       rule.outPath = pOptions.url;
@@ -19,12 +18,14 @@ module.exports = function createHttpRuleCreator(msgOptions){
     }
 
     // register hhtp rule in local express
-    try{
-      var thisFunc = msgOptions.app[ method ];
+    try {
+      var thisFunc = msgOptions.app[method];
 
       // TODO: multiple handlers (mw) should be implemented
       thisFunc.bind(msgOptions.app)(rule.outPath, argObj.handler); //app.get(url, (req, res) => {})
-    } catch(e){msgOptions.log('EXPRESS could not create route: ' + rule.outPath, e.message, e.stack);}
+    } catch (e) {
+      msgOptions.log('EXPRESS could not create route: ' + rule.outPath, e.message, e.stack);
+    }
     return rule;
   };
 };
